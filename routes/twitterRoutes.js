c4145dbf41188aeb9c11cdeed98a489698b2ff1b
thirumurugan7 <thiru82003@gmail.com>
2025-08-14 18:25:28 +0530

    Remove Twitter-related functionality and configuration files, including routes, controllers, and cron service. Update .gitignore to include .env file.

--- routes/twitterRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postTweet } = require('../controller/twitterController');
-
-router.post('/tweet', postTweet);
-
-module.exports = router;
